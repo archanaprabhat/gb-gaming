c7a3044363613dfdcb86216d9bd0fcc3612bfeef
archanaprabhat <archanaprabhath2002@gmail.com>
2023-11-16 15:57:36 +0530

    first commit

---
+++ gb1/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="w-screen h-screen bg-red-400">HIII</div>
+    </>
+  );
+}
+
+export default App;
